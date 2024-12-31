76f90dec2078a9daceb2c75e76c23b8fb6faa212
cedasie <conquete.asie@gmail.com>
2024-12-31 09:33:09 +0100

    add admin course CRUD

---
+++ app/api/courses/[courseId]/route.ts
@@ -0,0 +1,59 @@
+import { eq } from "drizzle-orm";
+import { NextResponse } from "next/server";
+import db from "@/db/drizzle";
+import { courses } from "@/db/schema";
+import { isAdmin } from "@/lib/admin";
+
+// GET
+export const GET = async (
+  req: Request,
+  { params }: { params: { courseId: number } }
+) => {
+  if (!isAdmin()) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, params.courseId),
+  });
+
+  return NextResponse.json(data);
+};
+
+// PUT
+export const PUT = async (
+  req: Request,
+  { params }: { params: { courseId: number } }
+) => {
+  if (!isAdmin()) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+
+  const body = await req.json();
+  const data = await db
+    .update(courses)
+    .set({
+      ...body,
+    })
+    .where(eq(courses.id, params.courseId))
+    .returning();
+
+  return NextResponse.json(data[0]);
+};
+
+// DELETE
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { courseId: number } }
+) => {
+  if (!isAdmin()) {
+    return new NextResponse("Unauthorized", { status: 403 });
+  }
+
+  const data = await db
+    .delete(courses)
+    .where(eq(courses.id, params.courseId))
+    .returning();
+
+  return NextResponse.json(data[0]);
+};
--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -6,9 +6,25 @@ import { isAdmin } from "@/lib/admin";
 
 export const GET = async () => {
   if (!isAdmin()) {
-    return new NextResponse("Unauthorized", { status: 403 });
+    return new NextResponse("Unauthorized", { status: 401 });
   }
   const data = await db.query.courses.findMany();
 
   return NextResponse.json(data);
+};
+
+export const POST = async (req: Request) => {
+  if (!isAdmin()) {
+    return new NextResponse("Unauthorized", { status: 401 });
+  }
+
+  const body = await req.json();
+  const data = await db
+    .insert(courses)
+    .values({
+      ...body,
+    })
+    .returning();
+
+  return NextResponse.json(data[0]);
 };
